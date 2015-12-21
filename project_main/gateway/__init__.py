ad9ca744fdc5cc76862ed67cf80150ee2d952acd
Bo <krist.jin.apply@gmail.com>
2015-12-21 10:23:32 -0500

    remove unused things

--- project_main/gateway/__init__.py
+++ project_main/gateway/__init__.py
@@ -1,13 +1,9 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from db_credentials import SQLALCHEMY_DATABASE_URI
 import logging
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
  
 logging.basicConfig(filename='gateway.log',level=logging.INFO,format='%(asctime)s --- %(message)s')
-
-# db = SQLAlchemy(app)
